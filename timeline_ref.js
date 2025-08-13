06baed52d1fb5320ddce46c084e482f9672f5de7
9vvin <9vvin.base@gmail.com>
2025-08-13 23:15:08 +0900

    record ref

---
+++ timeline_ref.js
@@ -0,0 +1,6 @@
+gsap
+  .timeline()
+  .from('#demo', { autoAlpha: 0 })
+  .from('#title', { opacity: 0, scale: 0, ease: 'back' })
+  .from('#freds img', { y: 160, stagger: 0.1, duration: 0.8, ease: 'back' })
+  .from('#time', { xPercent: 100, duration: 0.2 });
